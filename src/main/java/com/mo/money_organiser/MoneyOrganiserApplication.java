a81ed3efb469e7708f29d528683eaa2a1c81d84f
amit <amitsatpathy8@gmail.com>
2025-08-10 17:25:10 +0530

    Initial commit

---
+++ src/main/java/com/mo/money_organiser/MoneyOrganiserApplication.java
@@ -0,0 +1,13 @@
+package com.mo.money_organiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyOrganiserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyOrganiserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mo/money_organiser/MoneyOrganiserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mo.money_organiser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyOrganiserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
